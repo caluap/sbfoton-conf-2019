cb9576b92780f85b9d20b90ee8b8793e48a66135
Daniel Abrahao <daniel@preface.com.br>
2018-08-27 16:29:43 -0300

    Standard compiling script

---
+++ scripts/compile.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+if ! which harp > /dev/null; then
+	echo "harp command not found!"
+	exit 1
+fi
+
+cd $(dirname "$0")
+cd ..
+
+LOG=$(harp compile . dist 2>&1)
+EXIT="$?"
+
+if [ ! -z "$LOG" ]; then
+	echo "The compile command issued $(echo "$LOG" | wc -l) message(s)."
+	read -p "Print them? (Y/[n]) " choice
+	if [ "$choice" = "Y" -o "$choice" = "y" ]; then
+		echo "$LOG"
+	fi
+fi
+
+exit ${EXIT}
